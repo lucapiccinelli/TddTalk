05909631477349efc6b0f9b584e070ca8f96214f
Luca Piccinelli <luca.picci@gmail.com>
2021-05-22 14:28:29 +0200

    improved readability

--- BirhdayGreetings/EmployeesLineParser.cs
+++ BirhdayGreetings/EmployeesLineParser.cs
@@ -8,21 +8,43 @@ namespace BirthdayGreetings
     {
         public static Employee ParseLineToEmployee(string employeeLine)
         {
-            string[] employeeTokens = employeeLine.Split(",").Select(s => s.Trim()).ToArray();
-            if(employeeTokens.Length < 4) throw new ArgumentException($"Some information is missing on this line: {employeeLine}");
-            if(employeeTokens.Any(string.IsNullOrEmpty)) throw new ArgumentException($"Some information is missing on this line: {employeeLine}");
+            string[] employeeTokens = ReadTokens(employeeLine);
+            ValidateExpectedTokenNumber(employeeLine, employeeTokens, 4);
+            ValidateNotEmptyTokens(employeeLine, employeeTokens);
+            DateTime dateOfBirth = ValidateDate(employeeLine, employeeTokens[2]);
+
+            return new Employee(
+                firstname: employeeTokens[1],
+                lastname: employeeTokens[0],
+                dateOfBirth,
+                email: employeeTokens[3]);
+        }
+
+        private static DateTime ValidateDate(string employeeLine, string dateString)
+        {
             try
             {
-                return new Employee(
-                    employeeTokens[1],
-                    employeeTokens[0],
-                    DateTime.ParseExact(employeeTokens[2], "yyyy/MM/dd", CultureInfo.InvariantCulture),
-                    employeeTokens[3]);
+                return DateTime.ParseExact(dateString, "yyyy/MM/dd", CultureInfo.InvariantCulture);
             }
-            catch (FormatException e)
+            catch(FormatException e)
             {
                 throw new ArgumentException($"The date has an invalid format: {employeeLine}", e);
             }
+        }
+
+        private static void ValidateNotEmptyTokens(string employeeLine, string[] employeeTokens)
+        {
+            if (employeeTokens.Any(string.IsNullOrEmpty)) throw new ArgumentException($"Some information is missing on this line: {employeeLine}");
+        }
+
+        private static void ValidateExpectedTokenNumber(string employeeLine, string[] employeeTokens, int expectedTokens)
+        {
+            if (employeeTokens.Length < expectedTokens) throw new ArgumentException($"Some information is missing on this line: {employeeLine}");
+        }
+
+        private static string[] ReadTokens(string employeeLine)
+        {
+            return employeeLine.Split(",").Select(s => s.Trim()).ToArray();
         }
     }
 }
